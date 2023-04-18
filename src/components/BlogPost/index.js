6db737fd847bf2b4882b44f65ec90c9f39e8c6e9
sam98963 <szwilliamson01@gmail.com>
2023-04-18 15:43:01 +0100

    task 2 complete

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -10,13 +10,21 @@ imageAlt: string
 
 
 
-function BlogPost(blog) {
+function BlogPost() {
+  let blog = {
+    title: "My First Post",
+    author: "by Chris Meah",
+    datePosted: "10/11/2019",
+    content: "A structure used in most apps and games. It's a way to keep doing the same. While a condition is true, Or for one to twenty-two. If endless, for errors we blame ..........Loop",
+    imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGokeVnr-yPUrwsGJVwy_m_AL2j--gXJ-3g&usqp=CAU",
+    imageAlt: "A couple of coders.",
+  }
   return (<div>
-    <h1>My First Post</h1>
-    <h3>by Chris Meah</h3>
-    <p>10/11/2019</p>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGokeVnr-yPUrwsGJVwy_m_AL2j--gXJ-3g&usqp=CAU" alt="A couple of coders."/>
-    <p>A structure used in most apps and games. It's a way to keep doing the same. While a condition is true, Or for one to twenty-two. If endless, for errors we blame ..........Loop</p>
+    <h1>{blog.title}</h1>
+    <h3>{blog.author}</h3>
+    <p>{blog.datePosted}</p>
+    <img src={blog.imageSrc} alt={blog.imageAlt}/>
+    <p>{blog.content}</p>
    
   </div>);
 }
--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,11 +1,30 @@
 // Take in props of author and content to display the comment
+// write a function to take in author and return capitalised initials
 
 
-function Comment(author, content) {
+// Remove hard coding of author and content when input works!
+function firstInitials () {
+  let author = "Sam Williamson"
+  let words = author.split(" ");
+let initials = "";
+
+for (let i = 0; i < words.length; i++) {
+  let initial = words[i].charAt(0).toUpperCase();
+  initials += initial;
+}
+return initials;
+} 
+
+function Comment() {
+  let author = "Sam Williamson"
+let content = "Hello There"
+    const initials = firstInitials(author);
   return (<div>
-    <div>BL</div>
-    <h3>Ben Lee</h3>
-    <p>Hello, great post.</p>
+    <div>
+      <p>{initials}</p>
+    </div>
+    <h3>{author}</h3>
+    <p>{content}</p>
   </div>)
 }
 
