4f2e586bd826fcec2cdf159f7c7748eba20a4988
sam98963 <szwilliamson01@gmail.com>
2023-04-18 17:12:34 +0100

    mainly done, need to allow for author and comment to be shown in Comment component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,25 @@
 import BlogPost from "../BlogPost";
 import CommentList from "../CommentList";
 import CommentForm from "../CommentForm";
+import {useState} from "react"
+import {blog} from "../../data/blogs"
 function App() {
+  const [comments, setComments] = useState([])
+  function handleSubmit(author, content){
+    // create an object for the comment
+    // add it to the array of comments
+    const newComment = {
+      id: Math.random().toString(30),
+      author: author,
+      comment: content,
+    }
+    setComments([...comments, newComment])
+    console.log(comments)
+  }
   return (<>
-  <BlogPost />
-  <CommentList />
-  <CommentForm></CommentForm>
+  <BlogPost blog = {blog}/>
+  <CommentList comments = {comments}/>
+  <CommentForm handleSubmit = {handleSubmit}></CommentForm>
   </>)
 }
 
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -8,17 +8,7 @@ imageSrc: string
 imageAlt: string
 */
 
-
-
-function BlogPost() {
-  let blog = {
-    title: "My First Post",
-    author: "by Chris Meah",
-    datePosted: "10/11/2019",
-    content: "A structure used in most apps and games. It's a way to keep doing the same. While a condition is true, Or for one to twenty-two. If endless, for errors we blame ..........Loop",
-    imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGokeVnr-yPUrwsGJVwy_m_AL2j--gXJ-3g&usqp=CAU",
-    imageAlt: "A couple of coders.",
-  }
+function BlogPost({blog}) {
   return (<div>
     <h1>{blog.title}</h1>
     <h3>{blog.author}</h3>
