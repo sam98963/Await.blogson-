86437217523beb434ffa5cb37b1c69d69ab7ed98
sam98963 <szwilliamson01@gmail.com>
2023-04-21 19:03:52 +0100

    some more styling complete

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,8 @@ import CommentList from "../CommentList";
 import CommentForm from "../CommentForm";
 import {useState} from "react"
 import {blog} from "../../data/blogs"
+import "./App.css"
+
 function App() {
   const [comments, setComments] = useState([{author: "Sam Williamson", comment: "Wow this is so cool", id: "1"}])
   function handleSubmit(author, comment){
@@ -16,7 +18,25 @@ function App() {
     setComments([...comments, newComment])
   }
   return (<>
+  <nav>
+    <h1>Await.blogson()</h1>
+    <div className = "nav-bar-list">
+      <ul>
+        <div className="nav-list-item">
+        <li>Blog</li>
+        
+        <li>Comments</li>
+
+        <li>Add a Comment!</li></div>
+      </ul>
+    </div>
+  </nav>
+  <section className="blog-section">
+    <div className="blog-section-title">
+    <h1>Our Latest Blog!</h1>
+    </div>
   <BlogPost blog = {blog}/>
+  </section>
   <CommentList comments = {comments}/>
   <CommentForm handleSubmit = {handleSubmit}></CommentForm>
   </>)
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -7,14 +7,22 @@ content: string
 imageSrc: string
 imageAlt: string
 */
+import "./BlogPost.css"
 
 function BlogPost({blog}) {
-  return (<div>
-    <h1>{blog.title}</h1>
-    <h3>{blog.author}</h3>
-    <p>{blog.datePosted}</p>
-    <img src={blog.imageSrc} alt={blog.imageAlt}/>
-    <p>{blog.content}</p>
+  return (<div className="blog-post">
+    <div className="content-div">
+      <h1>{blog.title}</h1>
+      <p>{blog.content}</p>
+      <div className="blog-author-date">
+      <p><span>Blog Author: </span>{blog.author}</p>
+      <p><span>Date Posted: </span>{blog.datePosted}</p>
+      </div>
+    </div>
+    <div className="img-div">
+      <img src={blog.imageSrc} alt={blog.imageAlt}/>
+    </div>
+    
    
   </div>);
 }
